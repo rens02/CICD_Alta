5cf8454cedef017e27167ed8cd98f7da3b14e872
rens02 <cyberlearninghack@gmail.com>
2023-10-08 22:19:10 +0700

    config

---
+++ config/conn.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"Prioritas2/models"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	InitDB()
+	InitialMigration()
+}
+
+func InitDB() {
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	portStr := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		log.Fatalf("Invalid port in .env: %v", err)
+	}
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true",
+		username,
+		password,
+		host,
+		port,
+		name,
+	)
+	DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %s", err.Error())
+	}
+}
+
+func InitialMigration() {
+	DB.AutoMigrate(&models.User{}, &models.Books{}, &models.Blog{})
+}
